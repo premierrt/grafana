26a1fca907d4192f9965162a8caa00bf1965ddfe
dom_linux <dom_linux@ddddd.xy>
2018-09-19 23:08:10 +0200

    init

---
+++ src/main/java/boot_grafana/BootGrafanaApplication.java
@@ -0,0 +1,12 @@
+package boot_grafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGrafanaApplication.class, args);
+	}
+}
